3a7697531ff4cc62087691e7a959498ba3c596c4
Valery Semenenko <gearmobile@gmail.com>
2022-09-23 19:58:24 +0300

    feat(backend api): add graphql config

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,12 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { environment } from '../environments/environment';
 
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({ ...environment.connection })],
+  imports: [
+    TypeOrmModule.forRoot({ ...environment.connection }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      typePaths: ['./**/*.graphql'],
+      context: ({ req }) => ({ req }),
+      playground: true,
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppResolver],
 })
 export class AppModule {}
